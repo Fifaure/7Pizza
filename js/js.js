54b9c28be6860c3328740f80d07182b5f07b50f4
Fifaure <156931314+Fifaure@users.noreply.github.com>
2024-05-31 15:57:43 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,8 @@
+
+document.querySelectorAll('.menu-items a').forEach(item => {
+  item.addEventListener('click', event => {
+    document.getElementById('menu-toggle').checked = false;
+  });
+});
+
+
