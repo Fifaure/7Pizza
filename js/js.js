696dc123c74c2f0914939c191248d7ab10d6d4ea
Fifaure <156931314+Fifaure@users.noreply.github.com>
2024-05-31 16:07:17 +0200

    Delete js directory

--- js/js.js
+++
@@ -1,8 +0,0 @@
-
-document.querySelectorAll('.menu-items a').forEach(item => {
-  item.addEventListener('click', event => {
-    document.getElementById('menu-toggle').checked = false;
-  });
-});
-
-
